cdbb46244bb477e933e410f2b303fb061e8116db
alekssamos <aleks-samos@yandex.ru>
2020-08-31 21:37:28 +0300

    add example

---
+++ example/example1.py
@@ -0,0 +1,11 @@
+from yandexfreetranslate import YandexFreeTranslate
+yt = YandexFreeTranslate()
+print(yt.translate("auto", "ru", "Hello, my friend!"))
+
+yt.set_proxy("socks5", "socks.zaborona.help", 1488)
+
+with open("in.txt", "r", encoding="utf8") as f:
+	text = yt.translate("en", "ru", f.read())
+
+with open("out.txt", "w", encoding="utf8") as f:
+	f.write(text)
